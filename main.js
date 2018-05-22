379251e9d1591f54f0d3f66f3e6d0a13148908fe
Daniel S <magicgames9@gmail.com>
2018-05-22 21:57:17 +0300

    Now the bot can tell the weather :D

--- main.js
+++ main.js
@@ -6,16 +6,30 @@ const client = new Discord.Client();
 client.on("ready", () => {
     console.log(`${client.user.username} is ready.`)
 });
-
+let tips = ["If a guy asks you out, say no.", "Eat", "Go outside"];
 client.on("message", msg => {
     if (msg.author.bot) return; // if the author of the message is the bot itself, do nothing.
-    if (msg.author.username === "h̶ ̶i̶ ̶l̶ ̶l̶ ̶i̶ ̶e̶ (｡ಥ︿ಥ｡)") msg.reply("Hillie, stop."); //tells hillie to stop.
-    if (msg.author.username === "h̶ ̶i̶ ̶l̶ ̶l̶ ̶i̶ ̶e̶ (｡ಥ︿ಥ｡)") return; // if the message was sent from hillie, do nothing.
     if (msg.content.indexOf(config.prefix) !== 0) return; // if the message doesn't have the bot's prefix, do nothing.
     msg.content = msg.content.substr(6); // removes the bot's prefix, for better handling.
     msg.content = msg.content.toLocaleLowerCase(); // turn the message into lowercase, for better handling.
     console.log(msg.author.username);
-    if(msg.content === "hello") msg.reply("you're looking quite aesthetic today.");
-    if(msg.content === "weather") msg.reply(`${weather.getWeather()}`);
+    switch (msg.content) { // handling communication with the clients.
+        case "hello":
+        case "hey":
+        case "hi":
+            msg.reply("Hello handsome.");
+        break;
+        case "tip" :
+        case "tips" :
+            msg.reply(tips[Math.floor(Math.random(0, tips.length) * tips.length)]); // replying to the client with a random tip from the array "tips".
+        break;
+        case "weather":
+        msg.reply(`Working on it!. beep boop bap :D`)
+            weather.getWeather(result => {
+                msg.reply(`${result.currently.summary}, ${result.currently.temperature}`);
+            });
+        break;
+        };
 });
+
 client.login(config.token);
--- weather.js
+++ weather.js
@@ -1,10 +1,10 @@
-//TODO: put the weather function here and export it into the 'main.js' file.
-//TODO: also make it work.
+//TODO: make the weather request
 const https = require("https");
+const config = require("./config.json");
 
 module.exports = exports = {
-    getWeather: function(){
-        https.get("https://api.darksky.net/forecast/5e53b4cbe9686ba91db22667471617f4/32.109333,34.855499", res => {
+    getWeather: function(callback){
+        https.get(`https://api.darksky.net/forecast/${config.weatherKey}/32.109333,34.855499`, res => {
             let body = '';
             
             res.on('data', chunk =>{
@@ -14,8 +14,7 @@ module.exports = exports = {
             res.on('end', ()=>{
                 const response = JSON.parse(body);
                 console.log(response.currently);
-                return response;
-                // callback(response);
+                callback(response);
             });
         });
     }
