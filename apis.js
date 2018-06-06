6c79b0d16dd7563a2435fabc3cd19a68124ddde9
Daniel <daniel.meter0@gmail.com>
2018-06-06 12:48:37 +0300

    added chuck norris jokes

--- apis.js
+++ apis.js
@@ -3,7 +3,7 @@ const https = require("https");
 const config = require("./config.json");
 
 module.exports = exports = {
-    getWeather: function(callback){
+    getWeather: function(callback){ //weather function.
         https.get(`https://api.darksky.net/forecast/${config.weatherKey}/32.109333,34.855499`, res => {
             let body = '';
             
@@ -14,6 +14,21 @@ module.exports = exports = {
             res.on('end', ()=>{
                 const response = JSON.parse(body);
                 console.log(response.currently);
+                callback(response);
+            });
+        });
+    },
+
+    getChuckNorris: function(callback){ // chuck norris joke
+        https.get('https://api.chucknorris.io/jokes/random', res=>{
+            let body = '';
+
+            res.on('data', chunk =>{
+                body += chunk;
+            });
+
+            res.on("end", chunk =>{
+                const response = JSON.parse(body);
                 callback(response);
             });
         });
--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const config = require("./config.json");
-const weather = require("./weather");
+const apis = require("./apis.js");
 const client = new Discord.Client();
 
 client.on("ready", () => {
@@ -10,7 +10,7 @@ let tips = ["If a guy asks you out, say no.", "Eat", "Go outside"];
 client.on("message", msg => {
     if (msg.author.bot) return; // if the author of the message is the bot itself, do nothing.
     if (msg.content.indexOf(config.prefix) !== 0) return; // if the message doesn't have the bot's prefix, do nothing.
-    msg.content = msg.content.substr(6); // removes the bot's prefix, for better handling.
+    msg.content = msg.content.substr(1); // removes the bot's prefix, for better handling.
     msg.content = msg.content.toLocaleLowerCase(); // turn the message into lowercase, for better handling.
     console.log(msg.author.username);
     switch (msg.content) { // handling communication with the clients.
@@ -24,9 +24,15 @@ client.on("message", msg => {
             msg.reply(tips[Math.floor(Math.random(0, tips.length) * tips.length)]); // replying to the client with a random tip from the array "tips".
         break;
         case "weather":
-        msg.reply(`Working on it!. beep boop bap :D`)
-            weather.getWeather(result => {
-                msg.reply(`${result.currently.summary}, ${result.currently.temperature}`);
+            msg.reply(`beep bap boop`)
+            apis.getWeather(result => {
+                msg.reply(`${result.currently.summary}, ${Math.round((result.currently.temperature - 32) * 5/9)}`);
+            });
+        break;
+        case "chuck norris":
+            msg.reply("give me a second, I am thinking of a good one!");
+            apis.getChuckNorris(result =>{
+                msg.reply(result.value);
             });
         break;
         };
