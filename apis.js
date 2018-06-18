19b5dd6fcb9dab2a2816b3d86e2cd23120295ad0
Daniel S <magicgames9@gmail.com>
2018-06-18 20:20:53 +0300

    Added some more neat tips and gifs

--- apis.js
+++ apis.js
@@ -3,7 +3,7 @@ const https = require("https");
 const config = require("./config.json");
 
 module.exports = exports = {
-    getWeather: function(callback){ //weather function.
+    getWeather: function(callback){ //getting the weather, hardcoded to TLV.
         https.get(`https://api.darksky.net/forecast/${config.weatherKey}/32.109333,34.855499`, res => {
             let body = '';
             
@@ -13,12 +13,24 @@ module.exports = exports = {
 
             res.on('end', ()=>{
                 const response = JSON.parse(body);
-                console.log(response.currently);
                 callback(response);
             });
         });
     },
+    getGif: function(search, callback){ //getting some gifs
+        https.get(`https://api.giphy.com/v1/gifs/search?q=${search}&api_key=${config.giphyKey}&limit=2`, res => {
+            let body = '';
+            
+            res.on('data', chunk =>{
+                body += chunk;
+            });
 
+            res.on('end', ()=>{
+                const response = JSON.parse(body);
+                callback(response);
+            });
+        });
+    },
     getChuckNorris: function(callback){ // chuck norris joke
         https.get('https://api.chucknorris.io/jokes/random', res=>{
             let body = '';
--- main.js
+++ main.js
@@ -4,37 +4,46 @@ const apis = require("./apis.js");
 const client = new Discord.Client();
 
 client.on("ready", () => {
-    console.log(`${client.user.username} is ready.`)
+    console.log(`${client.user.username} is ready.`);
 });
-let tips = ["If a guy asks you out, say no.", "Eat", "Go outside"];
+let tips = ["If a guy asks you out, say no.", "Eat", "Go outside.", "Meet someone.", "If you're hungry, get something to eat.", "Drink some water. You stupid fucking bitch.", "Eat some fucking vegtables.", "Fucking do something, so you won't go to sleep crying over shit that you could have done and didn't end up doing.", "Learn from your mistakes."];
+//straight calls.
 client.on("message", msg => {
     if (msg.author.bot) return; // if the author of the message is the bot itself, do nothing.
     if (msg.content.indexOf(config.prefix) !== 0) return; // if the message doesn't have the bot's prefix, do nothing.
-    msg.content = msg.content.substr(1); // removes the bot's prefix, for better handling.
+    msg.content = msg.content.substr(6); // removes the bot's prefix, for better handling. actually try changing it to config.prefix.
     msg.content = msg.content.toLocaleLowerCase(); // turn the message into lowercase, for better handling.
     console.log(msg.author.username);
     switch (msg.content) { // handling communication with the clients.
         case "hello":
         case "hey":
         case "hi":
-            msg.reply("Hello handsome.");
+            msg.channel.sendMessage("Hello handsome.");
         break;
         case "tip" :
         case "tips" :
-            msg.reply(tips[Math.floor(Math.random(0, tips.length) * tips.length)]); // replying to the client with a random tip from the array "tips".
+            msg.channel.sendMessage(tips[Math.floor(Math.random(0, tips.length) * tips.length)]); // replying to the client with a random tip from the array "tips".
         break;
         case "weather":
-            msg.reply(`beep bap boop`)
+            msg.channel.sendMessage(`beep bap boop`)
             apis.getWeather(result => {
-                msg.reply(`${result.currently.summary}, ${Math.round((result.currently.temperature - 32) * 5/9)}`);
+                msg.channel.sendMessage(`${result.currently.summary}, ${Math.round((result.currently.temperature - 32) * 5 / 9)}`); //from fahrenheit to celsius
             });
         break;
         case "chuck norris":
-            msg.reply("give me a second, I am thinking of a good one!");
+            msg.channel.sendMessage("give me a second, I am thinking of a good one!");
             apis.getChuckNorris(result =>{
-                msg.reply(result.value);
+                msg.channel.sendMessage(result.value);
             });
         break;
+        };
+        //calls with options
+        if(msg.content.startsWith("gif" || "gifs")){
+            apis.getGif(/.+?\s(.+)/.exec(msg.content)[1], result => {
+                console.log(`client looked for ${/.+?\s(.+)/.exec(msg.content)[1]}`);
+                msg.channel.sendMessage(result.data[0].url)
+                msg.channel.sendMessage(result.data[1].url)
+            });
         };
 });
 
