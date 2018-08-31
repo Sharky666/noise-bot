4c1efe1ef596a063ef90a607930ad7f796453eaf
Daniel S <magicgames9@gmail.com>
2018-08-31 22:21:14 +0300

    added and fixed some stuff, also now you can control the bot from the terminal!

--- apis.js
+++ apis.js
@@ -1,7 +1,6 @@
 //TODO: make the weather request
 const https = require("https");
 const config = require("./config.json");
-
 module.exports = exports = {
     getWeather: function(callback){ //getting the weather, hardcoded to TLV.
         https.get(`https://api.darksky.net/forecast/${config.weatherKey}/32.109333,34.855499`, res => {
--- main.js
+++ main.js
@@ -3,18 +3,28 @@ const config = require("./config.json");
 const apis = require("./apis.js");
 const client = new Discord.Client();
 
+let tips = ["If a guy asks you out, say no.", "Go outside.", "Meet someone.", "If you're hungry, get something to eat.", "Drink some water. You stupid fucking bitch.", "Eat some fucking vegtables.", "Fucking do something, so you won't go to sleep crying over shit that you could have done and didn't end up doing.", "Learn from your mistakes."];
+const stdin = process.openStdin();
+let authorLastChannel = null;
+
 client.on("ready", () => {
     console.log(`${client.user.username} is ready.`);
 });
-let tips = ["If a guy asks you out, say no.", "Eat", "Go outside.", "Meet someone.", "If you're hungry, get something to eat.", "Drink some water. You stupid fucking bitch.", "Eat some fucking vegtables.", "Fucking do something, so you won't go to sleep crying over shit that you could have done and didn't end up doing.", "Learn from your mistakes."];
-//straight calls.
+
+//Terminal handler
+stdin.addListener("data", d =>{
+    if(authorLastChannel === null) return;
+    d = d.toString().trim();
+    authorLastChannel.sendMessage(d);
+});
+//Discord handler
 client.on("message", msg => {
-    if (msg.author.bot) return; // if the author of the message is the bot itself, do nothing.
+    if(msg.author.id === config.authorId);
+        authorLastChannel = msg.channel;
+    if (msg.author.bot) return; // if the author of the message is a bot, do nothing.
     if (msg.content.indexOf(config.prefix) !== 0) return; // if the message doesn't have the bot's prefix, do nothing.
-    msg.content = msg.content.substr(6); // removes the bot's prefix, for better handling. actually try changing it to config.prefix.
-    msg.content = msg.content.toLocaleLowerCase(); // turn the message into lowercase, for better handling.
-    console.log(msg.author.username);
-    switch (msg.content) { // handling communication with the clients.
+    const parsedMsg = parseMessageContent(msg.content);
+    switch (parsedMsg.cmd) { // handling communication with the clients.
         case "hello":
         case "hey":
         case "hi":
@@ -30,21 +40,30 @@ client.on("message", msg => {
                 msg.channel.sendMessage(`${result.currently.summary}, ${Math.round((result.currently.temperature - 32) * 5 / 9)}`); //from fahrenheit to celsius
             });
         break;
-        case "chuck norris":
+        case "chuck-norris":
             msg.channel.sendMessage("give me a second, I am thinking of a good one!");
             apis.getChuckNorris(result =>{
                 msg.channel.sendMessage(result.value);
             });
         break;
-        };
-        //calls with options
-        if(msg.content.startsWith("gif" || "gifs")){
-            apis.getGif(/.+?\s(.+)/.exec(msg.content)[1], result => {
-                console.log(`client looked for ${/.+?\s(.+)/.exec(msg.content)[1]}`);
+        case "gif":
+        case "gifs":
+            apis.getGif(parsedMsg.args.join(" "), result => {
+                console.log(`client looked for ${parsedMsg.args.join(" ")}`);
                 msg.channel.sendMessage(result.data[0].url)
                 msg.channel.sendMessage(result.data[1].url)
             });
+        break;
         };
 });
 
+function parseMessageContent(msg){
+    let data = msg.toLowerCase().split(" ");
+    let result = {
+        wakeWord: data[0],
+        cmd: data[1],
+        args: data.slice(2)
+    };
+    return result;
+};
 client.login(config.token);
